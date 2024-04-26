06a3e0048db856e64b7869cc70312f110c0f5f6b
axelle <kadance9720@gmail.com>
2024-04-26 19:24:45 +0200

    Cart button update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,40 +12,48 @@ function Cart({ cart, updateCart }) {
 
   return isOpen ? (
     <div className="jh-cart">
-      <div className="jh-cart-button">
-        <h2 className="jh-cartTitle">Panier</h2>
-        <button
-          className="jh-cart-toggle-button"
-          onClick={() => setIsOpen(false)}
-        >
-          <img src={closeIcon} alt="close icon" className="jh-cart-icon" />
-        </button>
-      </div>
+      <button
+        className="jh-cart-toggle-close-button"
+        onClick={() => setIsOpen(false)}
+      >
+        <img src={closeIcon} alt="close icon" className="jh-cart-icon" />
+      </button>
+      <h2 className="jh-cart-title">Panier</h2>
 
       {cart.length > 0 ? (
         <div>
           <ul>
             {cart.map(({ name, price, amount }, index) => (
               <div key={`${name}-${index}`}>
-                <li>
+                <li className="jh-cart-item">
                   {name} - {price}€ x {amount}
                 </li>
               </div>
             ))}
           </ul>
-          <p className="jh-cartTotal">Total: {total}€</p>
-          <button onClick={() => updateCart([])}>Vider le panier</button>
+          <h2 className="jh-cart-total">Total : {total}€</h2>
+          <div className="jh-cart-toggle-button-action">
+            <button
+              className="jh-cart-toggle-button"
+              onClick={() => updateCart([])}
+            >
+              Vider le panier
+            </button>
+            <button
+              className="jh-cart-toggle-button"
+              onClick={() => setIsOpen(false)}
+            >
+              Paiement
+            </button>
+          </div>
         </div>
       ) : (
         <div>Votre panier est vide</div>
       )}
     </div>
   ) : (
-    <div className="lmj-cart-closed">
-      <button
-        className="lmj-cart-toggle-button"
-        onClick={() => setIsOpen(true)}
-      >
+    <div className="jh-cart-closed">
+      <button className="jh-cart-toggle-button" onClick={() => setIsOpen(true)}>
         Ouvrir le panier
       </button>
     </div>
